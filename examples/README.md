f7ed1506ca03aa1eda2b62a28ac7c0e58e99f7a6
ClaudioMartino <claudiomartino.it@gmail.com>
2025-01-29 15:18:14 +0100

    Improved FMD script

---
+++ examples/README.md
@@ -0,0 +1,41 @@
+# Models
+
+We have implemented a python3 script based on our hymble scraping library to retrieve informations from the [Fashion Model Directory](https://www.fashionmodeldirectory.com/) (FMD). A similar work have been made in [1].
+
+## Methodology
+To run the script:
+```
+python3 fashionmodeldirectory.py
+```
+
+It will retrieve data from more than 15 thousand personal records (as of 2025) and look for inconsistencies. On our machine it run for about 4 hours.
+
+The retrieved data are:
+
+- Name and surname of the model
+- FMD page
+- Nationality
+- Birth date
+- Height in cm
+- Bust size in cm
+- Waist size in cm
+- Hip size in cm
+- Dress size (EU)
+- Shoes size (EU)
+- Color of the hair
+- Color of the eyes
+
+For example, this is the vector corresponding to [Monica Bellucci](https://www.fashionmodeldirectory.com/models/monica_bellucci/) (🇮🇹):
+```
+Monica Bellucci,https://www.fashionmodeldirectory.com/models/monica_bellucci/,Italian,September 30 1964,175.0,90.0,61.0,90.0,36,40,Dark brown,Brown
+```
+
+When values are unavailable or wrong, `NA` has been written.
+
+In the dataset men and women are mixed. Differences in size and proportions between male and female body have been studied [2], hence we have been trying to distinguish the sexes of the models looking at the distribution of the collected data.
+
+## References
+1. Topirceanu, A., & Udrescu, M. (2015, September). *FMNet: Physical Trait Patterns in the Fashion World*. In Network Intelligence Conference (ENIC), 2015 Second European (pp. 25-32). IEEE.
+2. Robinette, K.M., Churchill, T., & Mcconville, J.T. (1979). [*A Comparison of Male and Female Body Sizes and Proportions*](https://apps.dtic.mil/sti/pdfs/ADA074807.pdf).
+
+
--- examples/fashionmodeldirectory.py
+++ examples/fashionmodeldirectory.py
@@ -14,61 +14,137 @@ def move_ptr_after(text, string, i):
     i = i + len(string)
   return i
 
-def inch_cm(string, i):
-  # Look for third
-  i = move_ptr_after(html, string, i)
-  i = move_ptr_after(html, string, i)
-  i = move_ptr_after(html, string, i)
+def print_red(string):
+  print('\033[91m' + string + '\033[0m')
 
-  string = "<span class=\"DataPoint\">"
-  i = move_ptr_after(html, string, i)
-  i3 = move_ptr(html, string, i)
-  i3 = i3 + len(string)
-  string = "</span>"
-  i2 = move_ptr(html, string, i)
-  i4 = move_ptr(html, string, i3)
-
-  # Inches
-  if (html[i:i2].replace('.', '', 1).isdigit()):
-    inch = (float(html[i:i2]))
+def read_inches(string):
+  if (string.replace('.', '', 1).isdigit()):
+    # Simple number
+    inch = float(string)
+  elif (string.replace('&#039;', '', 1).replace('&quot;', '', 1).replace('.','').isdigit()):
+    # Feet and inches (1' = 12")
+    # Example: 5&#039;10&quot = 5'10"
+    tmp = string.replace('&quot;', '', 1).split('&#039;')
+    feet = float(tmp[0])
+    inch = float(tmp[1])
+    inch = feet * 12 + inch
   else:
-    inch = 0
+    print_red("Can't read inches: " + string)
+    inch = 'NA'
+  return inch
 
-  # Centimeters
-  if (html[i3:i4].replace('.', '', 1).isdigit()):
-    cm = (float(html[i3:i4]))
-  elif (html[i3:i4-2].replace('.', '', 1).isdigit()):
-    cm = (float(html[i3:i4-2]))
+def read_cm(string):
+  if (string.replace('.', '', 1).isdigit()):
+    cm = float(string)
+    # Simple number
+  elif (string[:-2].replace('.', '', 1).isdigit()):
+    # Remove CM
+    cm = float(string[:-2])
   else:
-    cm = 0
+    print_red("Can't read cm: " + string)
+    cm = 'NA'
 
-  return [inch, cm]
+  return cm
 
+def read_inches_cm(string_inches, string_cm):
+  inches = read_inches(string_inches)
+  cm = read_cm(string_cm)
 
+  if(isinstance(inches, float) and isinstance(cm, float)):
+    # Sometimes cm and inch are reversed by mistake
+    # Example: https://www.fashionmodeldirectory.com/models/karen_pillet/
+    if(cm < inches):
+      print_red("Inch and cm reversed")
+      inches, cm = cm, inches
 
+    # 1 centimeter is equal to 0.3937007874 inches
+    if(abs(inches - cm * 0.3937007874) > 5):
+      print_red('Inch / cm BIG mismatch: ' + str(inches) + ' - ' + str(cm) )
+      inches = "NA"
+      cm = "NA"
 
+    # Check if values > 0
+    if(inches <= 0):
+      inches = "NA"
+    if(cm <= 0):
+      cm = "NA"
+
+  return inches, cm
+
+def data_point(string, i):
+  # Look for third occurence of input string
+  i = move_ptr_after(html, string, i)
+  i = move_ptr_after(html, string, i)
+  i = move_ptr_after(html, string, i)
+
+  # Example:
+  # <span class="DataPoint">35.5</span>
+  # <span class="Separator">/</span>
+  # <span class="DataPoint">90</span>
+
+  st = "<span class=\"DataPoint\">"
+  b1 = move_ptr_after(html, st, i)
+  b2 = move_ptr_after(html, st, b1)
+  #b3 = move_ptr_after(html, st, b2)
+
+  st = "</span>"
+  e1 = move_ptr(html, st, b1)
+  e2 = move_ptr(html, st, b2)
+  #e3 = move_ptr(html, st, b3)
+
+  us_string = html[b1:e1]
+  eu_string = html[b2:e2]
+  #uk_string = html[b3:e3]
+
+   # Treat dress and shoes differently, they are simpler
+  inch_cm_strings = ["Height", "Bust", "Waist", "Hips"]
+  if (string in inch_cm_strings):
+    inch, cm = read_inches_cm(us_string, eu_string)
+    return cm
+  else:
+    if(eu_string.isdigit()):
+      return int(eu_string)
+    elif(eu_string.replace('.','',1).isdigit()):
+      return float(eu_string)
+    elif(eu_string == ''):
+      return 'NA'
+    else:
+      return eu_string
+
+def hair_eyes(string, i):
+  # Look for third occurence of input string
+  i = move_ptr_after(html, string, i)
+  i = move_ptr_after(html, string, i)
+  i = move_ptr_after(html, string, i)
+
+  # Example: <div class="Wrap">Dark brown</div>
+  color = scraping.find_string(html[i:], '<div class=\"Wrap\">', '</div>')
+
+  return color
 
 ### MAIN ###
 
 models_per_page = 12
 
-letters = list(string.ascii_uppercase)
-offset = 23
-
 # Loop over letters
+letters = list(string.ascii_uppercase)
+print(letters)
+print(len(letters))
+
+offset = 0
 for l in letters[offset:]:
   # create url list
   model_names = []
   model_urls  = []
 
-  # Read all webpages
+  # Read webpages
   url_cnt = 0
   while(1):
     u = "https://www.fashionmodeldirectory.com/models/search/alphabetical_order/" + l + "/?start=" + str(url_cnt)
     html = scraping.read_webpage(u)
 
+    # Look for models in page
     strings = scraping.find_more_strings(html, '<article class=\"PhotoModule TitleInside', '</article>')
-
     for st in strings:
       model_name = scraping.find_string(st, '<h3 itemprop=\"accountablePerson\">', '</h3>')
       url = scraping.find_string(st, '<div class="Link"><a href=\"', '\" itemprop=\"url\">')
@@ -106,16 +182,24 @@ for l in letters[offset:]:
     #if cnt != models_per_page:
     #  break
 
+    # Update url
     url_cnt += models_per_page
 
+  tot_models = len(model_urls)
+  print("[TOTAL] " + str(tot_models))
+
+  # Open output file
   f = open("models" + l + ".csv", 'w')
   writer = csv.writer(f)
 
+  # Loop over models and read features
   for cnt, url_m in enumerate(model_urls):
+    print(str(cnt + 1) + "/" + str(tot_models))
+    # Open web page
     # Example: "https://www.fashionmodeldirectory.com/models/toma_aardenburg/",
     html = scraping.read_webpage(url_m)
 
-    # Nationality
+    # Read nationality
     string = "<div class=\"Nationality\" itemprop=\"nationality\">"
     nationality = scraping.find_string(html, string, '</div>')
 
@@ -132,15 +216,22 @@ for l in letters[offset:]:
     if tmp != -1:
       i = tmp
 
+    # Colors
+    hair = hair_eyes('Hair', i)
+    eyes = hair_eyes('Eyes', i)
+
     # Measurements
-    [height_inch, height_cm] = inch_cm("Height", i)
-    [bust_inch, bust_cm]     = inch_cm("Bust", i)
-    [waist_inch, waist_cm]   = inch_cm("Waist", i)
-    [hips_inch, hips_cm]     = inch_cm("Hips", i)
+    height_cm = data_point("Height", i)
+    bust_cm   = data_point("Bust", i)
+    waist_cm  = data_point("Waist", i)
+    hips_cm   = data_point("Hips", i)
+    dress     = data_point("Dress", i)
+    shoes     = data_point("Shoes", i)
 
     # Write csv row
-    csv_out = [model_names[cnt], model_urls[cnt], nationality, date, height_cm, bust_cm, waist_cm, hips_cm]
-    writer.writerow(csv_out)
+    csv_out = [model_names[cnt], model_urls[cnt], nationality, date, height_cm, bust_cm, waist_cm, hips_cm, dress, shoes, hair, eyes]
     print(csv_out)
-  
+    writer.writerow(csv_out)
+ 
+  # Close csv file
   f.close()
